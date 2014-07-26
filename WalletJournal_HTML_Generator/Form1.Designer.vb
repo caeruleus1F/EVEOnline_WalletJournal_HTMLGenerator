6f5f2ade81218380ed9fc129036ac6d5ae5c6248
Garrett Bates <gbates31@gmail.com>
2014-07-26 00:09:34 -0700

    initial commit

---
+++ WalletJournal_HTML_Generator/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblPull = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(120, 48)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Begin"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Next pull at:"
+        '
+        'lblPull
+        '
+        Me.lblPull.AutoSize = True
+        Me.lblPull.Location = New System.Drawing.Point(100, 9)
+        Me.lblPull.Name = "lblPull"
+        Me.lblPull.Size = New System.Drawing.Size(0, 17)
+        Me.lblPull.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(314, 83)
+        Me.Controls.Add(Me.lblPull)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "EVE HTML Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblPull As System.Windows.Forms.Label
+
+End Class
---
+++ WalletJournal_HTML_Generator/Form1.vb
@@ -0,0 +1,148 @@
+﻿Imports System.Net
+Imports System.Xml
+Imports System.IO
+
+Public Class Form1
+    Dim web As New WebClient()
+    Dim walletTransactions As New List(Of XElement)
+    Dim timestamps As New List(Of String)
+    Dim refID As New List(Of Integer)
+    Dim transactionSender As New List(Of String)
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        GetDocument()
+        Timer1.Enabled = True
+    End Sub
+
+    Public Sub GetDocument()
+        Dim url As String = "https://api.eveonline.com/char/WalletJournal.xml.aspx?keyID=3431927&vCode=A6L29SRsXNWljWTMBbECa9BQ8NXYHY4pNcCAkiQeyBxQxUGzXGSbqYrX9gnyRg7f&characterID=94201985"
+        AddHandler web.DownloadStringCompleted, AddressOf DownloadComplete
+
+        Try
+            web.DownloadStringAsync(New Uri(url))
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Public Sub DownloadComplete(ByVal sender As Object, ByVal e As DownloadStringCompletedEventArgs)
+        RemoveHandler web.DownloadStringCompleted, AddressOf DownloadComplete
+        Dim xmlDoc As XElement
+        Dim writer As New StreamWriter("index.html")
+        Dim strReason As String = Nothing
+        Dim dblAmount As Double = 0.0
+        Dim currentTime = Date.Now(), cachedUntil As Date
+        Dim dateDiff As TimeSpan
+        Dim msResult As Integer = 0
+
+        Try
+            xmlDoc = XElement.Parse(e.Result)
+            walletTransactions = xmlDoc...<row>.ToList()
+            Dim quickwrite As New StreamWriter("xml.xml")
+            quickwrite.WriteLine(xmlDoc.ToString())
+            quickwrite.Close()
+
+            ' this portion controls the interval at which the API endpoint is polled
+            ' 10 seconds are added to make sure the CCP server has the XML doc ready
+            currentTime = currentTime.ToUniversalTime()
+            cachedUntil = xmlDoc...<cachedUntil>.Value
+            dateDiff = cachedUntil - currentTime
+            msResult = dateDiff.TotalMilliseconds
+            lblPull.Text = DateAdd(DateInterval.Second, 10, cachedUntil).ToString() & " UTC"
+            Timer1.Enabled = False
+            Timer1.Interval = msResult + 10000
+            Timer1.Enabled = True
+
+            writer.WriteLine("<!DOCTYPE HTML><html><head><link href='style.css' rel='stylesheet'><meta charset='UTF-8'></head><body>")
+            writer.WriteLine("<table><thead><tr><td>Timestamp</td><td>Sender</td><td>Receiver</td><td>Amount</td><td>Reason</td></tr></thead><tbody>")
+
+            For counter As Integer = 0 To walletTransactions.Count - 1 Step 1
+
+                If walletTransactions.Item(counter).@ownerName2.ToString().Equals("Thirtyone Organism") Or
+                    walletTransactions.Item(counter).@ownerName1.ToString().Equals("Thirtyone Organism") Then
+                    Continue For
+                End If
+
+                dblAmount = CDbl(walletTransactions.Item(counter).@amount)
+
+                If walletTransactions.Item(counter).@reason.ToString().Contains("IDS") And dblAmount >= 1000000000 Then
+                    writer.WriteLine("<tr><td class='t'>" & AddTime(walletTransactions.Item(counter).@date) & "</td>" &
+                                     "<td>" & walletTransactions.Item(counter).@ownerName2.ToString() & "</td>" &
+                                     "<td>" & walletTransactions.Item(counter).@ownerName1.ToString() & "</td>" &
+                                     "<td>-" & (dblAmount * 4).ToString("N2") & "</td>" &
+                                     "<td class='w'>4x winner!</td></tr>")
+
+                ElseIf walletTransactions.Item(counter).@reason.ToString().Contains("IDS") And dblAmount >= 500000000 Then
+                    writer.WriteLine("<tr><td class='t'>" & AddTime(walletTransactions.Item(counter).@date) & "</td>" &
+                                     "<td>" & walletTransactions.Item(counter).@ownerName2.ToString() & "</td>" &
+                                     "<td>" & walletTransactions.Item(counter).@ownerName1.ToString() & "</td>" &
+                                     "<td>-" & (dblAmount * 3).ToString("N2") & "</td>" &
+                                     "<td class='w'>3x winner!</td></tr>")
+
+                ElseIf walletTransactions.Item(counter).@reason.ToString().Contains("IDS") And dblAmount >= 200000000 Then
+                    writer.WriteLine("<tr><td class='t'>" & AddTime(walletTransactions.Item(counter).@date) & "</td>" &
+                                     "<td>" & walletTransactions.Item(counter).@ownerName2.ToString() & "</td>" &
+                                     "<td>" & walletTransactions.Item(counter).@ownerName1.ToString() & "</td>" &
+                                     "<td>-" & (dblAmount * 2.5).ToString("N2") & "</td>" &
+                                     "<td class='w'>2.5x winner!</td></tr>")
+
+                ElseIf walletTransactions.Item(counter).@reason.ToString().Contains("IDS") And dblAmount >= 50000000 Then
+                    writer.WriteLine("<tr><td class='t'>" & AddTime(walletTransactions.Item(counter).@date) & "</td>" &
+                                     "<td>" & walletTransactions.Item(counter).@ownerName2.ToString() & "</td>" &
+                                     "<td>" & walletTransactions.Item(counter).@ownerName1.ToString() & "</td>" &
+                                     "<td>-" & (dblAmount * 2).ToString("N2") & "</td>" &
+                                     "<td class='w'>2x winner!</td></tr>")
+
+                    ' ElseIf walletTransactions.Item(counter).@reason.ToString().Contains("IDS") And dblAmount >= 10000000 Then
+                    '     writer.WriteLine("<tr><td class='t'>" & AddTime(walletTransactions.Item(counter).@date) & "</td>" &
+                    '                      "<td>" & walletTransactions.Item(counter).@ownerName2.ToString() & "</td>" &
+                    '                      "<td>" & walletTransactions.Item(counter).@ownerName1.ToString() & "</td>" &
+                    '                      "<td>-" & (dblAmount * 1.5).ToString("N2") & "</td>" &
+                    '                      "<td class='w'>1.5x winner!</td></tr>")
+                End If
+
+                If walletTransactions.Item(counter).@reason.ToString().Contains("DESC: ") Then
+                    strReason = walletTransactions.Item(counter).@reason.ToString()
+                    strReason = strReason.Replace("DESC: ", "")
+                    walletTransactions.Item(counter).@reason = strReason
+                End If
+
+                writer.WriteLine("<tr><td class='t'>" & walletTransactions.Item(counter).@date.ToString() & "</td>" &
+                                 "<td>" & walletTransactions.Item(counter).@ownerName1.ToString() & "</td>" &
+                                 "<td>" & walletTransactions.Item(counter).@ownerName2.ToString() & "</td>" &
+                                 "<td>" & dblAmount.ToString("N2") & "</td>" &
+                                 "<td>" & walletTransactions.Item(counter).@reason.ToString() & "</td></tr>")
+
+            Next
+
+            writer.WriteLine("</tbody></table>*Information on this page can take up to 30 minutes to update.</body></html>")
+            writer.Close()
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Public Function AddTime(ByVal iTime As Date) As String
+        Dim dateString As String = Nothing
+        Dim addedSeconds As Integer = 0
+
+        Randomize()
+        addedSeconds = 14
+        iTime = iTime.AddSeconds(addedSeconds)
+
+        dateString = Format(iTime, "yyyy-MM-dd HH:mm:ss")
+
+        Return dateString
+    End Function
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        GetDocument()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        web.Proxy() = Nothing
+        Timer1.Interval = 60000
+    End Sub
+End Class
